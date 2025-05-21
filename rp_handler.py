e17f007d0223f2129ec53187bfaad2f8e160bc88
ofir ozeri <ofiroz91@gmail.com>
2025-05-21 18:14:37 +0300

    ready to test runpod inference

--- rp_handler.py
+++ rp_handler.py
@@ -9,14 +9,15 @@ async def ws_server():
             # …here you would call handle_client() from eval_gr00t_so100.py …
     return await websockets.serve(echo, "0.0.0.0", 8765)
 
-def handler(event):
+# RunPod passes a job dict
+def handler(job):
     # 1) boot the WebSocket server
     asyncio.get_event_loop().create_task(ws_server())
 
     # 2) publish IP & port
     ip  = os.environ.get("RUNPOD_HTTP_IP")
     port = os.environ.get("RUNPOD_TCP_PORT_8765", 8765)
-    runpod.serverless.progress_update({"ip": ip, "port": int(port)})
+    runpod.serverless.progress_update(job, {"ip": ip, "port": int(port)})
 
     # 3) block until shutdown
     asyncio.get_event_loop().run_forever()
